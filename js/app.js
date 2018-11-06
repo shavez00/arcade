93080bca8513088c464a0713bc58045f91279734
shavez00@yahoo.com <>
2018-11-06 11:40:26 -0800

    removed comments from Udacity

--- js/app.js
+++ js/app.js
@@ -1,22 +1,12 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.x =x;
     this.y = y;
     this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x =+ this.speed * dt;
     
     if (this.x > 510) {
@@ -33,14 +23,9 @@ Enemy.prototype.update = function(dt) {
     }
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 var Player = function (x, y) {
     this.x = x;
@@ -76,9 +61,6 @@ Player.prototype.handleInput = function (keyPress) {
         }, 600)
     };
 };
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
 var allEnemies = [];
 var enemyLocation = [63, 147, 230];
@@ -90,10 +72,6 @@ enemyLocation.forEach(function (locationY) {
 
 var player = new Player(202, 405);
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
