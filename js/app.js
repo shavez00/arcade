a02de6556d86f41fee7ca5fdd88f0382dedffbca
shavez00 <shavez00@CORP.AMAZONWORKSPACES.COM>
2018-11-06 11:33:08 -0800

    Initial attempt at game

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x =x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +17,20 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x =+ this.speed * dt;
+    
+    if (this.x > 510) {
+        this.x = -50;
+        this.speed = 100 + Math.floor(Math.random() * 222);
+    }
+    
+    if (player.x < this.x + 80 &&
+            player.x + 80 > this.x &&
+            player.y < this.y + 60 &&
+            60+ player.y > this.y) {
+        player.x = 202;
+        player.y = 405;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,10 +42,53 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function (x, y) {
+    this.x = x;
+    this.y = y;
+    this.player = 'images/char-boy.png';
+};
 
+Player.prototype.update = function (dt) {
+    
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+};
+
+Player.prototype.handleInput = function (keyPress) {
+    if (keyPress == 'left' && this.x > 0) {
+        this.x -= 102;
+    };
+    if (keyPress == 'right' && this.x < 405) {
+        this.x += 102;
+    };
+    if (keyPress == 'up' && this.y > 0) {
+        this.y -= 83;
+    };
+    if (keyPress == 'down' && this.y < 405) {
+        this.y += 83;
+    };
+    if (this.y < 0) {
+        setTimeout(function () {
+            player.x = 202;
+            player.y = 405;
+        }, 600)
+    };
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+var allEnemies = [];
+var enemyLocation = [63, 147, 230];
+
+enemyLocation.forEach(function (locationY) {
+    enemy = new Enemy(0, locationY, -200);
+    allEnemies.push(enemy);
+});
+
+var player = new Player(202, 405);
 
 
 
